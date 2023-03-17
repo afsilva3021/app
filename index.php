8403da9584ed3241d310d421307169b285066640
Alex Felix <85413825+afsilva3021@users.noreply.github.com>
2023-03-17 15:16:09 -0300

    atualizado logo para index

--- index.php
+++ index.php
@@ -2,15 +2,40 @@
 <html lang="pt-br">
   <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Login e moto transp</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <link rel="stylesheet" type="text/css" href="./css/logo.css">
     </head>
   <body>
     
+    <div class="container loat-sm-start">
+<form>
+  <div class="row g-3 align-items-center">
+  <div class="col-auto"> 
+    <label for="exampleInputEmail6" class="form-label">Email Login</label>
+    <input type="email" class="form-control" id="exampleInputEmail6" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text" style="color: white;">Caso nao saiba o email Favor contata o suport.</div>
+    </div>  
+</div>
+    <br>
+  <div class="row g-3 align-items-center">
+    <div class="col-auto">  
+  <label for="exampleInputPassword6" class="form-label">Senha</label>
+    <input type="password" class="form-control" id="exampleInputPassword6">
+    <div id="emailHelp" class="form-text" style="color: white;">Caso nao saiba o email Favor contata o suport.</div>
+    </div>  
+</div>
+<br>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Salvar dados</label>
+  </div>
+  <button type="submit" class="btn btn-outline-info">Submit</button>
+</form>
 
-
+</div>
 
 
 
